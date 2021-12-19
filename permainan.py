0ad9bfafbdd35756c52b90aa08e2537aff7244f2
Andrean2305 <andreanhasan@gmail.com>
2021-12-19 20:07:48 +0700

    a

---
+++ permainan.py
@@ -0,0 +1,19 @@
+clicks= 104
+the_evolve= 98.74109999999999
+the_upgrade= 6
+the_cost= 50
+the_dream= 1
+the_level= 111
+the_evolving= 64.36342999999998
+the_prestige= 167
+the_prestige_prize= 293.32400000000075
+
+#This is the default[if you want to do a new game]
+#clicks = 0 
+#the_evolve = 50
+#the_upgrade = 1
+#the_cost = 50
+#the_dream = 1 
+#the_level = 1 
+#the_evolving = 50
+#the_prestige = 1
